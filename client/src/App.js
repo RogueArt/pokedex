cbd4efbdbdc1b3e68b5101224bb9dc02d12e5658
RogueArt <altoraries@gmail.com>
2021-08-15 14:09:57 -0700

    Dynamically render Pokemon information on each call; implement infinite scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -2,44 +2,129 @@ import axios from 'axios'
 
 import logo from './logo.svg'
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
+
+// Material UI components
+// import {
+//   FormControl,
+//   InputLabel,
+//   Input,
+//   FormHelperText,
+// } from '@material-ui/core'
+import Button from '@material-ui/core/Button'
+
+import PokemonEntry from './PokemonEntry.js'
+
+// TO-DO: Figure out what to do with dependency array
+// TO-DO: Clean up API call code so that it's more generic
+// TO-DO: Don't pass as much data to PokemonEntry
+// TO-DO: Refactor use effect code so it's less cursed in using state change
 
 function App() {
-  const [data, setData] = useState(null)
-  const [pokeData, setPokeData] = useState(null)
+  const limit = 20
+  const [offset, setOffset] = useState(0)
+  const [pokeData, setPokeData] = useState([])
+  const [atBottom, setAtBottom] = useState(false)
 
-  // useEffect(() => {
-  //   fetch('/api')
-  //     .then(res => res.json())
-  //     .then(data => setData(data.message))
-  // }, [])
+  // Make requests to get data for first 20 pokemon
+  async function requestPokemonData(idx) {
+    return axios.get(`https://pokeapi.co/api/v2/pokemon/${idx}`)
+  }
 
-  async function fetchPokeData() {
-    // Get URLs for first twenty pokemon
-    const { data: pokemonURLs } = await axios
-      .get('https://pokeapi.co/api/v2/pokemon/') // Make a GET request for first 20 pokemon
-      .catch(console.error) // Log any errors to console
+  // Get the images for first 20 pokemon as well
+  function getPokemonImageLink(idx) {
+    // Pad with zeroes to keep as three digit number
+    const paddedIdx = `00${idx}`.slice(-3)
+    const imageLink = `http://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIdx}.png`
+    return imageLink
+  }
 
-    // Make API calls to get data on first twenty pokemon
+  async function fetchPokemonData(limit, offset) {
+    // Grab the next `limit` pokemon's data, in our case that's 20 at a time
+    const dataPromiseArray = new Array(limit)
+      .fill(null)
+      .map((_, idx) => requestPokemonData(idx + offset + 1))
 
+    // Send all 20 requests at the same time, wait for it to come back
+    const responseData = await Promise.all(dataPromiseArray)
+    const pokemonData = responseData.map(res => res.data)
 
-    console.log(data)
+    setPokeData([...pokeData, ...pokemonData])
+    setOffset(offset + limit)
   }
 
   useEffect(() => {
-    axios.get('/api').then(data => setData(data.message))
-    fetchPokeData()
-  }, [])
+    fetchPokemonData(limit, offset)
+
+    const onScroll = function () {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        setAtBottom(true)
+      }
+      setAtBottom(false)
+    }
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [atBottom])
+
+  function renderPokemon() {
+    if (pokeData == null) return 'Loading...'
+
+    return pokeData.map(singlePokemon => {
+      // Gather data for our props
+      const { name, id, abilities, weight, height } = singlePokemon
+      const image = getPokemonImageLink(id)
+
+      // Render the component using props
+      const props = { name, id, abilities, weight, height, image }
+
+      return <PokemonEntry key={id} {...props} />
+    })
+  }
+
+  function renderMore() {
+    fetchPokemonData(limit, offset)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? 'Loading...' : data}</p>
-        <p>Pokemon: {pokeData ? pokeData : 'Waiting for Pokedata'}</p>
-      </header>
+      {/* <FormControl>
+        <InputLabel htmlFor="email">Email Address</InputLabel>
+        <Input id="email"></Input>
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <Input id="password"></Input>
+      </FormControl> */}
+      <Button
+        size="small"
+        variant="contained"
+        color="primary"
+        onClick={() => renderMore()}
+      >
+        Render More
+      </Button>
+      <div className="test">{renderPokemon()}</div>
     </div>
   )
 }
 
 export default App
+
+/*
+  {<header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>{!data ? 'Loading...' : data}</p>
+    <p>Pokemon: {pokeData ? pokeData : 'Waiting for Pokedata'}</p>
+  </header>}
+*/
+
+// async function fetchPokeData() {
+//   // Get URLs for first twenty pokemon
+//   const { data: pokemonURLs } = await axios
+//     .get('https://pokeapi.co/api/v2/pokemon/') // Make a GET request for first 20 pokemon
+//     .catch(console.error) // Log any errors to console
+
+//   // Make API calls to get data on first twenty pokemon
+
+//   console.log(data)
+// }
---
+++ client/src/PokemonEntry.js
@@ -0,0 +1,99 @@
+/* .App {
+  text-align: center;
+} */
+
+// Material UI components
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+// Material UI icons
+import Favorite from '@material-ui/icons/Favorite'
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder'
+
+import { useState } from 'react'
+import axios from 'axios'
+
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 240,
+  },
+})
+
+// Helper function to capitalize string
+function capitalize(s) {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+function PokemonEntry({ name, abilities, id, weight, height, image }) {
+  const [isFavorite, setIsFavorite] = useState(false)
+  
+  const classes = useStyles()
+
+  // Process props that we receive
+  const abilityNames = abilities.map(ability =>
+    capitalize(ability.ability.name)
+  )
+
+  function showFavoriteIcon() {
+    return isFavorite ? <Favorite /> : <FavoriteBorder /> 
+  }
+  
+  function handleFavoriteChange() {
+    // Set it to opposite of what it was before
+    setIsFavorite(!isFavorite)
+
+    // Make a request to the backend updating favorites
+    if (isFavorite) axios.post('/favorites', id)
+  }
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={image} title={name} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {capitalize(name)}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+          <Typography variant="body2" color="textPrimary" component="p">
+            <b>ID:</b> {id}
+          </Typography>
+          <Typography variant="body2" color="textPrimary" component="p">
+            <b>Abilities:</b> {abilityNames.join(', ')}
+          </Typography>
+          <Typography variant="body2" color="textPrimary" component="p">
+            <b>Height:</b> {height}
+          </Typography>
+          <Typography variant="body2" color="textPrimary" component="p">
+            <b>Weight:</b> {weight}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button startIcon={showFavoriteIcon()} color="secondary" size="small" onClick={() => setIsFavorite(!isFavorite)}>
+          Favorite
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default PokemonEntry

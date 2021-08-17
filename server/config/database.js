96237c851384a4c4f08dc1edc329ae230293a9fd
RogueArt <altoraries@gmail.com>
2021-08-17 12:54:08 -0700

    Made minor changes to backend logic

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-require('dotenv').config();
+require('dotenv').config()
 
 /**
  * -------------- DATABASE ----------------
@@ -9,29 +9,27 @@ require('dotenv').config();
 /**
  * Connect to MongoDB Server using the connection string in the `.env` file.  To implement this, place the following
  * string into the `.env` file
- * 
+ *
  * DB_STRING=mongodb://<user>:<password>@localhost:27017/database_name
- */ 
+ */
 
-const conn = process.env.MONGO_URI;
+const conn = process.env.MONGO_URI
 
 const connection = mongoose.createConnection(conn, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 console.log('Connected to Mongoose!')
 
 // Creates simple schema for a User.  The hash and salt are derived from the user's given password when they register
 const UserSchema = new mongoose.Schema({
-    username: String,
-    hash: String,
-    salt: String,
-    admin: Boolean
-});
+  username: String,
+  hash: String,
+  salt: String,
+  favorites: Object,
+}, { minimize: false })
 
-
-const User = connection.model('User', UserSchema);
-
+const User = connection.model('User', UserSchema)
 
 // Expose the connection
-module.exports = connection;
+module.exports = connection
